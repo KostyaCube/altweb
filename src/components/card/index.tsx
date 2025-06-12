c096c4336a992c2e139b8e20c4f845c639f4aed2
KostyaCube <psychocube91@gmail.com>
2025-06-12 13:55:26 +0600

    cards desktop added

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { SoftwareCard } from '../../pages/cards';
+import arrow from '../../assets/icons/arrow.svg';
+import star from '../../assets/icons/star.svg';
+import circleCheck from '../../assets/icons/circle-check.svg';
+import lock from '../../assets/icons/lock.svg';
 
-function Card() {
-  return <div>Card</div>;
+import './styles.css';
+
+function Card(props: SoftwareCard) {
+  const {
+    title,
+    imgSrc,
+    rating,
+    reviewsCount,
+    price,
+    official,
+    universal,
+    url,
+    description,
+  } = props;
+
+  return (
+    <Link to={url}>
+      <div className="wrapper">
+        <div className="card-info">
+          <img src={imgSrc} alt={title} />
+          <span className="price">{price}</span>
+
+          <div className="title-wrapper">
+            <div className="title">
+              {title}
+              <img src={arrow} alt="arrow" />
+            </div>
+
+            {universal && (
+              <div className="universal">
+                <img src={circleCheck} alt="check"></img> This software is
+                designed to work with any site
+              </div>
+            )}
+
+            <div className="badges">
+              <div className="rating">
+                {rating} <img src={star} alt="star" />
+              </div>
+
+              <div className="reviews">{reviewsCount} Reviews</div>
+
+              {official && (
+                <div className="official">
+                  <img src={lock} alt="lock" />
+                  Official software
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <span className="description">{description}</span>
+      </div>
+    </Link>
+  );
 }
 
 export default Card;
--- src/pages/cards/index.tsx
+++ src/pages/cards/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import Card from '../../components/card';
+import jsIcon from '../../assets/img/img-1.svg';
+import lockIcon from '../../assets/img/img-2.svg';
+import puppeteerIcon from '../../assets/img/img-3.svg';
+import './styles.css';
 
 export type SoftwareCard = {
   title: string;
@@ -9,50 +13,66 @@ export type SoftwareCard = {
   url: string;
   official?: boolean;
   universal?: boolean;
+  description?: string;
 };
 
 const cards: SoftwareCard[] = [
   {
     title: '2Captcha solver',
-    imgSrc: '/src/assets/img/img (1).svg',
+    imgSrc: lockIcon,
     rating: 4.6,
     reviewsCount: 456,
     price: 'Free',
     official: true,
     universal: true,
-    url: 'https://example.com/',
+    url: 'card/id',
+    description:
+      'Extension for the Google Chrome browser that automatically recognizes captchas such as ReCaptcha V2, V3, GeeTest, Arkose Labs FunCaptcha, Human Captcha and others.',
   },
   {
     title: 'Puppeteer plugin to solve reCAPTCHAs automatically',
-    imgSrc: '/src/assets/img/img (2).svg',
+    imgSrc: puppeteerIcon,
     rating: 4.6,
     reviewsCount: 456,
     price: '$67',
     universal: true,
-    url: 'https://example.com/',
+    url: 'card/id',
+    description:
+      'Solves reCAPTCHAs automatically, using a single line of code.',
   },
   {
     title: '2captcha-javascript',
-    imgSrc: '/src/assets/img/img (3).svg',
+    imgSrc: jsIcon,
     rating: 4.6,
     reviewsCount: 456,
     price: 'Free',
     universal: true,
-    url: 'https://example.com/',
+    url: 'card/id',
+    description:
+      'JavaScript npm package for easy integration with the API of 2captcha captcha solving service to bypass recaptcha, funcaptcha, geetest and solve any other captchas.',
   },
   {
     title: 'Puppeteer plugin to solve reCAPTCHAs automatically',
-    imgSrc: '/src/assets/img/img (2).svg',
+    imgSrc: puppeteerIcon,
     rating: 4.6,
     reviewsCount: 456,
     price: '$67',
-    universal: true,
-    url: 'https://example.com/',
+    universal: false,
+    url: 'card/id',
+    description:
+      'Extension for the Google Chrome browser that automatically recognizes captchas such as ReCaptcha V2, V3, GeeTest, Arkose Labs FunCaptcha, Human Captcha and others.',
   },
 ];
 
 function Cards() {
-  return <div>Cards</div>;
+  return (
+    <div className="container">
+      <h2>Software</h2>
+      {cards.map((card) => (
+        <Card key={card.title} {...card} />
+      ))}
+    </div>
+  );
 }
 
 export default Cards;
